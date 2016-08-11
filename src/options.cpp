4e321be48303871e2fab4c39aba60185e21715f6
logan42 <logan42@linuxmail.org>
2016-08-11 16:56:15 -0700

    Adding numeric contstraint when parsing command line

--- src/options.cpp
+++ src/options.cpp
@@ -10,6 +10,100 @@ namespace po = boost::program_options;
 
 Options options;
 
+template<typename T>
+class NumericConstraint : public TCLAP::Constraint<T>
+{
+protected:
+    bool lt, lte, gt, gte;
+    T lower, upper;
+public:
+    NumericConstraint()
+        : lt(false),
+        lte(false),
+        gt(false),
+        gte(false),
+        lower(0),
+        upper(0)
+    {}
+
+    NumericConstraint<T> * set_gt(T lower)
+    {
+        assert(!gt);
+        assert(!gte);
+        gt = true;
+        this->lower = lower;
+        return this;
+    }
+
+    NumericConstraint<T> * set_gte(T lower)
+    {
+        assert(!gt);
+        assert(!gte);
+        gte = true;
+        this->lower = lower;
+        return this;
+    }
+
+    NumericConstraint<T> * set_lt(T upper)
+    {
+        assert(!lt);
+        assert(!lte);
+        lt = true;
+        this->upper = upper;
+        return this;
+    }
+
+    NumericConstraint<T> * set_lte(T upper)
+    {
+        assert(!lt);
+        assert(!lte);
+        lte = true;
+        this->upper = upper;
+        return this;
+    }
+
+    virtual std::string description() const
+    {
+        std::string var_name = "x";
+        bool l = lt || lte;
+        bool g = gt || gte;
+        if (!l && !g) return var_name;
+        std::ostringstream oss;
+        if (l) {
+            if (g) {
+                oss << lower;
+                oss << (gt ? " <" : "<=");
+            }
+            oss << var_name;
+            oss << (lt ? "<" : "<=");
+            oss << upper;
+        }
+        else {
+            // g && !l
+            oss << var_name;
+            oss << (gt ? ">" : ">=");
+            oss << lower;
+        }
+        return oss.str();
+}
+
+    virtual std::string shortID() const
+    {
+        return this->description();
+    }
+
+    virtual bool check(const T& value) const
+    {
+        bool satisfies = true; 
+        if (gt && !(value > lower)) satisfies = false;
+        if (gte && !(value >= lower)) satisfies = false;
+        if (lt && !(value < upper)) satisfies = false;
+        if (lte && !(value <= upper)) satisfies = false;
+        return satisfies;
+    }
+};
+
+
 void parse_options(int argc, const char * argv[])
 {
     typedef TCLAP::ValueArg<std::string> TS;
@@ -33,11 +127,15 @@ void parse_options(int argc, const char * argv[])
     TS svm_query_arg("", "svm_query", "The name of the query_id prefix  in the input SVM file.", false, "qid", "string", cmd);
     TN default_raw_feature_type_arg("", "default_raw_feature_type", "When parsing a TSV or SVM file, Defines the default type of the feature values to expect. For more details, see src\\types.h, enum RawFeatureType.", false, 1, "size_t", cmd);
     TN random_seed_arg("", "random_seed", "Initial seed for random numbers generator. Set to 0 for randomized seed.", false, 1, "size_t", cmd);
-    TF input_sample_rate_arg("", "input_sample_rate", "Subsample only a given fraction of input documents.Useful for testing.", false, 1, "float", cmd);
+    NumericConstraint<float_t> input_sample_rate_con; input_sample_rate_con.set_gte(0)->set_lte(1);
+    TF input_sample_rate_arg("", "input_sample_rate", "Subsample only a given fraction of input documents.Useful for testing.", false, 1, &input_sample_rate_con, cmd);
     TF base_score_arg("", "base_score", "Base score of the model.", false, 0, "float", cmd);
-    TN bucket_max_bits_arg("", "bucket_max_bits", "Maximum number of bits to represent feature values. Any feature will be bucketized into at most 2^bucket_max_bits values. Possible values: 1..16.", false, 12, "size_t", cmd);
-    TF sparsity_threshold_arg("", "sparsity_threshold", "Feature sparsity threshold. Features that are sparser than this will be encoded in sparse format.", false, (float_t)0.1, "float", cmd);
-    TF initial_tail_size_arg("", "initial_tail_size", "Initial tail size for fast sparse features.", false, (float_t)0.03, "float", cmd);
+    NumericConstraint<size_t> bucket_max_bits_con; bucket_max_bits_con.set_gt(0)->set_lte(16);
+    TN bucket_max_bits_arg("", "bucket_max_bits", "Maximum number of bits to represent feature values. Any feature will be bucketized into at most 2^bucket_max_bits values. Possible values: 1..16.", false, 12, &bucket_max_bits_con, cmd);
+    NumericConstraint<float_t> sparsity_threshold_con; sparsity_threshold_con.set_gte(0)->set_lte(1);
+    TF sparsity_threshold_arg("", "sparsity_threshold", "Feature sparsity threshold. Features that are sparser than this will be encoded in sparse format.", false, (float_t)0.1, &sparsity_threshold_con, cmd);
+    NumericConstraint<float_t> initial_tail_size_con; initial_tail_size_con.set_gte(0)->set_lte(1);
+    TF initial_tail_size_arg("", "initial_tail_size", "Initial tail size for fast sparse features.", false, (float_t)0.03, &initial_tail_size_con, cmd);
     TS sparse_feature_version_arg("", "sparse_feature_version", "Defines which implementation of the sparse features to use.", false, "auto", "string", cmd);
     TN n_threads_arg("", "n_threads", "Number of threads to use for computation", false, 0, "size_t", cmd);
     TS cost_function_arg("", "cost_function", "Cost function and objective to solve. Possible values: regression, binary_classification, lambda_rank@N.", false, "", "string", cmd);
@@ -46,12 +144,16 @@ void parse_options(int argc, const char * argv[])
     TN n_leaves_arg("", "n_leaves", "Number of leaves per tree.", false, 0, "size_t", cmd);
     TN max_depth_arg("", "max_depth", "Maximum depth of a tree in the ensemble. Set to 0 to disable.", false, 0, "size_t", cmd);
     TN n_trees_arg("", "n_trees", "Number of trees in the ensemble.", false, 0, "size_t", cmd);
-    TN min_node_docs_arg("", "min_node_docs", "Minimum number of documents in a node.", false, 1, "size_t", cmd);
-    TF min_node_hessian_arg("", "min_node_hessian", "Minimum cumulative value of Hessian in a node.", false, (float_t)1.0, "float", cmd);
-    TF learning_rate_arg("", "learning_rate", "Learning rate, defines the step size coefficient.", false, (float_t)0.1, "float", cmd);
+    NumericConstraint<size_t> min_node_docs_con; min_node_docs_con.set_gt(0);
+    TN min_node_docs_arg("", "min_node_docs", "Minimum number of documents in a node.", false, 1, &min_node_docs_con, cmd);
+    NumericConstraint<float_t> min_node_hessian_con; min_node_hessian_con.set_gt(0);
+    TF min_node_hessian_arg("", "min_node_hessian", "Minimum cumulative value of Hessian in a node.", false, (float_t)1.0, &min_node_hessian_con, cmd);
+    NumericConstraint<float_t> learning_rate_con; learning_rate_con.set_gt(0);
+    TF learning_rate_arg("", "learning_rate", "Learning rate, defines the step size coefficient.", false, (float_t)0.1, &learning_rate_con, cmd);
     //TODO:Quadratic spread
     //("quadratic_spread", po::value<bool>(&options.quadratic_spread)->default_value(false), "Defines the formula for computing spread value of a split. If false, then absolute value of a difference between average gradients is used. If true, then difference of variances is used.")
-    TF regularization_lambda_arg("", "regularization_lambda", "Regularization parameter for quadratic spread.", false, (float_t)1.0, "float", cmd);
+    NumericConstraint<float_t> regularization_lambda_con; regularization_lambda_con.set_gte(0);
+    TF regularization_lambda_arg("", "regularization_lambda", "Regularization parameter for quadratic spread.", false, (float_t)1.0, &regularization_lambda_con, cmd);
     TB tree_debug_info_switch("", "tree_debug_info", "Whether to store debug information in the output ensemble.", cmd, false);
 
     TS input_tree_arg("", "input_tree", "For evaluation: input file containing a trained ensemble.", false, "", "string", cmd);
